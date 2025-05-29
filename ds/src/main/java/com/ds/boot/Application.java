db6d593bf716b3d79aebef80188c854c647e6dfc
11596 <ds@qq.com>
2025-05-29 16:18:58 +0800

    @EnableTransactionManagement

--- ds/src/main/java/com/ds/boot/Application.java
+++ ds/src/main/java/com/ds/boot/Application.java
@@ -2,12 +2,14 @@ package com.ds.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author ds
  * @date 2024/12/25
  * @description
  */
+@EnableTransactionManagement
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
