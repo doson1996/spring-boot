08f3d7799aadb48a79110519ff2cf0001204ec65
11596 <1@qq.com>
2026-03-25 16:51:51 +0800

    @EnableScheduling

--- ds/src/main/java/com/ds/boot/Application.java
+++ ds/src/main/java/com/ds/boot/Application.java
@@ -11,11 +11,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * 		1.注册SchedulingConfiguration @EnableScheduling -> @Import(SchedulingConfiguration.class) -> SchedulingConfiguration
  *		2.在该配置类注册ScheduledAnnotationBeanPostProcessor（ScheduledAnnotationBeanPostProcessor 实现了MergedBeanDefinitionPostProcessor）
  *		3.在ScheduledAnnotationBeanPostProcessor#postProcessAfterInitialization中收集所有带有@Scheduled注解的方法，
- *		并且将方法封装成ScheduledMethodRunnable（ScheduledAnnotationBeanPostProcessor#processScheduled）
+ *		并且将方法封装成ScheduledMethodRunnable {@link org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor#processScheduled}
  *		4.在ScheduledAnnotationBeanPostProcessor#postProcessAfterInitialization中将ScheduledMethodRunnable封装成Task(如CronTask、FixedDelayTask等),
  *		并且将Task添加到scheduledTasks中
  *		5.最后通过ContextRefreshedEvent事件触发ScheduledAnnotationBeanPostProcessor#finishRegistration方法，
  *		最终执行ScheduledTaskRegistrar的afterPropertiesSet方法调用scheduleTasks方法，开始调度任务
+ *
+ *		通过EnableScheduling注解注入了SchedulingConfiguration，在此配置类中又声明了ScheduledAnnotationBeanPostProcessor,在后置方法中去扫描bean的方法有没有
+ * 		Scheduled注解，如果有，将当前bean和方法封装成ScheduledMethodRunnable（以便需要时在run方法中通过反射执行当前方法），然后根据注解的值封装成对应
+ * 		的task(如FixedDelayTask)添加到任务列表中，这一步就完成了任务的收集，最后监听ContextRefreshedEvent事件，调用finishRegistration->this.registrar.afterPropertiesSet()
+ * 	    ->scheduleTasks() 开启对各种任务的调度
  */
 @EnableScheduling
 @SpringBootApplication
